edc2a8b52b98acba2ff90533d587cc142c355304
iSAM2016 <1228901986@qq.com>
2016-11-08 09:50:28 +0800

    each

---
+++ learn-underscore.js
@@ -0,0 +1,141 @@
+//2016年11月6日18:13:10
+/**
+*JavaScript实用库，提供了一整套函数式编程的实用功能，但是没有扩展任何JavaScript内置对象。它是这个问
+*/
+//函数整体
+
+//IIFE 将window穿入进去
+//JS中(function(){xxx})()
+/*
+* 包围函数（function(){})的第一对括号向脚本返回未命名的函数，随后一对空括号立即执行返回的未命名函数，括号内为匿名函数的参数。
+* 其他写法
+* (function () {  code  } ()); 
+* !function () {  code  } ();
+* ~function () {  code  } ();
+* -function () {  code  } ();
+* +function () {  code  } ();
+  函数声明：  function name(){}
+  函数表达式：var fnname = function(){}
+  匿名函数：  function(){} 匿名函数属于函数表达式
+
+  两者最主要的区别是： 一： 变量提升
+                       二： 函数表达式可以在后面添加扩看，但是命名函数不行吧
+
+                       function fnName(){
+                            alert('Hello World');
+                        }();//error
+
+                        function(){
+                            console.log('Hello World');    
+                        }();//error  没有赋值
+
+                        而（）、！、+、-、=等运算符，都将函数声明转换成函数表达式，
+                        消除了javascript引擎识别函数表达式和函数声明的歧义，告诉
+                        javascript引擎这是一个函数表达式，不是函数声明，可以在后面
+                        加括号，并立即执行函数的代码。
+
+                        (function(a){
+                            console.log(a);   //firebug输出123,使用（）运算符
+                        })(123);
+
+                        (function(a){
+                            console.log(a);   //firebug输出1234，使用（）运算符
+                        }(1234));
+
+    note: line 50
+          line 117  
+*/
+
+(function(){
+//基本配置
+    var  root = this;
+/**
+* 没有研究套 
+*/
+     var _ = function(obj) {
+    console.log('root')
+    if (obj instanceof _) return obj;
+    if (!(this instanceof _)) return new _(obj);
+    this._wrapped = obj;
+  };
+/**
+* --没有研究套 
+*/
+   
+
+    if( typeof exports !== "undefined" ){
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = _;
+        }
+        exports._ = _;
+    }else{
+
+        root._ = _
+    }
+
+    var getLength = function(collection){
+     return collection.length
+     }
+
+    var MAX_ARRAY_INDEX = Math.pow(2,53) - 1
+      var isArrayLike = function(collection){
+          var length = getLength( collection )  
+          return  typeof length === "number" && length >=0 && length  <= MAX_ARRAY_INDEX;   
+      }
+
+    var optimizeCb = function(func, context, argCount) {
+        if (context === void 0) return func;
+        switch (argCount == null ? 3 : argCount) {
+          case 1: return function(value) {
+            return func.call(context, value);
+          };
+          case 2: return function(value, other) {
+            return func.call(context, value, other);
+          };
+          case 3: return function(value, index, collection) {
+            return func.call(context, value, index, collection);
+          };
+          case 4: return function(accumulator, value, index, collection) {
+            return func.call(context, accumulator, value, index, collection);
+          };
+    }
+    return function() {
+      return func.apply(context, arguments);
+    };
+  };
+
+//集合函数 (数组 或对象)
+_.each = _.forEach = function(obj, iteratee, context){
+      iteratee = optimizeCb(iteratee, context);
+     if(isArrayLike(obj)){
+        for(i = 0, length = obj.length; i < length; i++){
+
+            iteratee(obj[i],i, obj)
+        }
+     }else{
+       
+        /**
+        * Object.keys，该方法返回对象属性一个数组
+        */
+      
+        var keys = Object.keys(obj)
+        console.log(keys)
+        for (i = 0, length = keys.length; i < length; i++) {
+        iteratee(obj[keys[i]], keys[i], obj);
+      }
+
+     }
+     return obj
+}
+/*遍历list中的所有元素，按顺序用遍历输出每个元素。
+        如果传递了context参数，则把iteratee绑定到context对象上。每次
+    调用iteratee都会传递三个参数：(element, index, list)。
+        如果没有context 参数，optimizeCb直接返回iteratee,在iteratee上
+    进行循环遍历  
+        如果list是个JavaScript对象，iteratee的参数是 (value, key, list))。
+    返回list以方便链式调用。（愚人码头注：如果存在原生的forEach方法，
+    Underscore就使用它代替。）
+*/
+
+
+}.call(this))
---
+++ underscore.js
@@ -0,0 +1,120 @@
+//     Underscore.js 1.8.3
+//     http://underscorejs.org
+//     (c) 2009-2015 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+//     Underscore may be freely distributed under the MIT license.
+
+(function(el) {
+
+  // Baseline setup
+  // --------------
+
+  // Establish the root object, `window` in the browser, or `exports` on the server.
+  var root = this;
+
+
+
+ 
+  // Create a safe reference to the Underscore object for use below.
+  var _ = function(obj) {
+    console.log('root')
+    if (obj instanceof _) return obj;
+    if (!(this instanceof _)) return new _(obj);
+    this._wrapped = obj;
+  };
+
+  // Export the Underscore object for **Node.js**, with
+  // backwards-compatibility for the old `require()` API. If we're in
+  // the browser, add `_` as a global object.
+  if (typeof exports !== 'undefined') {
+    if (typeof module !== 'undefined' && module.exports) {
+      exports = module.exports = _;
+    }
+    exports._ = _;
+  } else {
+    root._ = _;
+  }
+
+
+
+  // Internal function that returns an efficient (for current engines) version
+  // of the passed-in callback, to be repeatedly applied in other Underscore
+  // functions.
+  var optimizeCb = function(func, context, argCount) {
+     //func === iteratee
+     //context === context
+     //argcount
+
+     //function(obj, iteratee, context) 
+
+    if (context === void 0) return func;
+    switch (argCount == null ? 3 : argCount) {
+      case 1: return function(value) {
+        return func.call(context, value);
+      };
+      case 2: return function(value, other) {
+        return func.call(context, value, other);
+      };
+      case 3: return function(value, index, collection) {
+        return func.call(context, value, index, collection);
+      };
+      case 4: return function(accumulator, value, index, collection) {
+        return func.call(context, accumulator, value, index, collection);
+      };
+    }
+    return function() {
+      return func.apply(context, arguments);
+    };
+  };
+
+  // A mostly-internal function to generate callbacks that can be applied
+  // to each element in a collection, returning the desired result — either
+  // identity, an arbitrary callback, a property matcher, or a property accessor.
+
+  // An internal function for creating assigner functions.
+ 
+
+  var property = function(key) {
+    return function(obj) {
+      return obj == null ? void 0 : obj[key];
+    };
+  };
+
+  // Helper for collection methods to determine whether a collection
+  // should be iterated as an array or as an object
+  // Related: http://people.mozilla.org/~jorendorff/es6-draft.html#sec-tolength
+  // Avoids a very nasty iOS 8 JIT bug on ARM-64. #2094
+  var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
+  var getLength = property('length');
+  var isArrayLike = function(collection) {
+    var length = getLength(collection);
+    return typeof length == 'number' && length >= 0 && length <= MAX_ARRAY_INDEX;
+  };
+
+  // Collection Functions
+  // --------------------
+
+  // The cornerstone, an `each` implementation, aka `forEach`.
+  // Handles raw objects in addition to array-likes. Treats all
+  // sparse array-likes as if they were dense.
+  _.each = _.forEach = function(obj, iteratee, context) {
+    console.log(_)
+    iteratee = optimizeCb(iteratee, context);
+    console.log( iteratee)
+    var i, length;
+    if (isArrayLike(obj)) {
+      for (i = 0, length = obj.length; i < length; i++) {
+        iteratee(obj[i], i, obj);
+      }
+    } else {
+      var keys = _.keys(obj);
+      for (i = 0, length = keys.length; i < length; i++) {
+        iteratee(obj[keys[i]], keys[i], obj);
+      }
+    }
+    return obj;
+  };
+
+  
+
+
+}.call(this));
