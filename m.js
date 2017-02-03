f6eac2f41ef79ee6d53b96e706b388770fcf5725
iSAM2016 <1228901986@qq.com>
2017-02-04 07:43:17 +0800

    add tree

--- m.js
+++ m.js
@@ -20,5 +20,55 @@
     }else{
         root._ = _;
     }
-console.log(_)
+
+    /**
+    * 获取集合长度
+    */
+    var getLength = function(collection){
+        return collection.length
+    }
+
+    /**
+    * 判断是否为数组，利用数组的长度
+    */
+    var MAX_ARRAY_INDEX = Math.pow(2,53)-1
+    var isArrayLike = function(collection){
+        var length = getLength(collection)
+        return typeof length === 'number' && length >=0 && length <= MAX_ARRAY_INDEX
+    }
+
+    var optimizeCb = function(func, context, argCount){
+        if(context === void 0) return func;
+        switch (argCount == null ? 3 : argCount){
+            case 3: return function(value, index, collection){
+                 return func.call(context,value,index,collection)
+            }
+        }
+    }
+
+    _.each = _.forEach = function(obj, iteratee, context){
+        iteratee = optimizeCb(iteratee,context);
+        var i ,length;
+        if(isArrayLike(obj)){
+            for(i=0; i<obj.length;i++){
+                iteratee(obj[i],i,obj)
+            }
+        }else{
+            var keys = _.keys(obj);
+            console.log(keys)
+            for(i=0, length = keys.length; i < length;i++){
+                iteratee(obj[keys[i]],keys[i],obj)
+            }
+       } 
+    }
+
+    _.keys = function(obj){
+        console.log(nativeKeys(obj))
+        if(nativeKeys) return nativeKeys(obj);
+    }
+
+    _.isObject = function(){
+
+    }
+
 }.call(this))
